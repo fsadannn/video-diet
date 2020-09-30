a8db2200e8724b48e234a05dbcb37e8e75f1d3cb
Frank S <fsadannn@gmail.com>
2020-09-30 18:55:23 -0400

    add intel accel encoding/decoding

--- video_diet/__init__.py
+++ video_diet/__init__.py
@@ -24,8 +24,8 @@ BAR_FMT = u'{desc}{desc_pad}{percentage:3.0f}%|{bar}| {count:{len_total}.1f}/{to
 COUNTER_FMT = u'{desc}{desc_pad}{count:.1f} {unit}{unit_pad}' + \
               u'[{elapsed}, {rate:.2f}{unit_pad}{unit}/s]{fill}'
 
-CONVERT_COMMAND_10Bits = 'ffmpeg -progress pipe:1 -i "{source}" -map 0 -map -v -map V -c:v libx265 -x265-params crf=26:profile=main10 -c:a aac -y "{dest}"'
-CONVERT_COMMAND = 'ffmpeg -progress pipe:1 -i "{source}" -map 0 -map -v -map V -c:v libx265 -crf 26 -c:a aac -y "{dest}"'
+CONVERT_COMMAND_10Bits = 'ffmpeg -hwaccel qsv -progress pipe:1 -i "{source}" -map 0 -map -v -map V -c:v hevc_qsv -load_plugin hevc_hw -profile main10 -global_quality 26 -c:a aac -y "{dest}"'
+CONVERT_COMMAND = 'ffmpeg -hwaccel qsv -progress pipe:1 -i "{source}" -map 0 -map -v -map V -c:v hevc_qsv -load_plugin hevc_hw -global_quality 26 -c:a aac -y "{dest}"'
 
 def convert_file(source: str, dest: str):
     stream = ffmpeg.input(source)
